e5745da06b74e880c07839223f1ff1f6069b9bda
varunnallamothu <varunchowdary1996@gmail.com>
2017-05-23 15:19:21 +0530

    Add files via upload

---
+++ 3. First.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# Author: kumar
+# usage : saying hello to the world.
+# logs  : 16-05-2017 - kumar -first program
+#       : 18-05-2019 - new modification
+print ("hello world !!!")
